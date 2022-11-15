371a9f7ea8d651288cad80310ac4b52ec3e349ea
nestor <nestordesabille4@gmail.com>
2022-11-15 10:26:43 +0800

    finish version 1

--- script.js
+++ script.js
@@ -15,37 +15,49 @@ function buttonAssignment(){
         if(button.parentElement.parentElement.className == "numbers-btns"){
             button.addEventListener('click',()=>{
                 if((strDisplay == "Press any digit to start")||(RESULT.textContent!="0")){
-                    strDisplay = ""
+                    strDisplay = "";
+                    RESULT.textContent = "0";
                 }
-                strDisplay += button.id;
+                let buttonId = button.id;
+                if(buttonId == "zero"){
+                    buttonId = "0";
+                }
+                else if(buttonId == "dot"){
+                    buttonId = ".";
+                }
+                strDisplay += buttonId;
                 DISPLAY.textContent = strDisplay;
+                
             });
         }
         if(button.parentElement.className == "misc-btns"){
             button.addEventListener('click',()=>{
                 strDisplay = "Press any digit to start"
                 DISPLAY.textContent = strDisplay;
+                RESULT.textContent = "0";
             });
         }
         if(button.parentElement.className == "operands-btns"){
             button.addEventListener('click',()=>{
-                if(button.id == "btnequals"){
-                    calculate(1);
-                }
-
-                if(cntOperator == 1){
-                    calculate(0);
-                    strDisplay = strResult + " " + button.id + " ";
-                    DISPLAY.textContent = strDisplay;
-                    cntOperator++;
-                }
-                
-                else if(strDisplay != "Press any digit to start"){
-                    strDisplay = strDisplay + " " + button.id + " ";
-                    DISPLAY.textContent = strDisplay;
-                    cntOperator++;
-                }
-                
+                if(RESULT.textContent == "0"){
+                    if(button.id == "btnequals"){
+                        calculate(1);
+                        RESULT.textContent = strResult;
+                    }
+                    else{
+                        if(cntOperator == 1){
+                            calculate(0);
+                            strDisplay = strResult + " " + button.id + " ";
+                            DISPLAY.textContent = strDisplay;
+                            cntOperator++;
+                        }
+                        else if(strDisplay != "Press any digit to start"){
+                            strDisplay = strDisplay + " " + button.id + " ";
+                            DISPLAY.textContent = strDisplay;
+                            cntOperator++;
+                        }
+                    }
+                }   
             });
         }
     });
@@ -54,26 +66,23 @@ function buttonAssignment(){
 function calculate(x){
     const STRINGARRAY = strDisplay.split(" ");
     const OPERATORS = ["+","-","X","%"];
-
     let nums = []
     let currOperator = ""
     let answer = 0;
 
     STRINGARRAY.forEach(ele => {
-        OPERATORS.every(operator => {
+        OPERATORS.forEach(operator => {
             if(ele == operator){
                 currOperator = ele;
-                return false;
             }
         });
+        
         if(ele != currOperator){
-            let newNum = parseInt(ele);
+            let newNum = parseFloat(ele);
             nums.push(newNum);
-            console.log(nums);
         }
         
     });
-
     switch (currOperator){
         case "+":
             answer = nums[0] + nums[1];
@@ -93,7 +102,7 @@ function calculate(x){
 
     strResult = answer.toString();
     if(x==1){
-        RESULT.textContent = strResult;
+        
     }
 
     cntOperator = 0;
